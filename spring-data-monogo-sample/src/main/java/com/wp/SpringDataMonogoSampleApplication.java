20faa345cf603c871689952afe30d82dc87aaea6
wanpeng <18717792412@163.com>
2020-06-01 22:38:48 +0800

    mongodb示例

---
+++ spring-data-monogo-sample/src/main/java/com/wp/SpringDataMonogoSampleApplication.java
@@ -0,0 +1,13 @@
+package com.wp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataMonogoSampleApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( SpringDataMonogoSampleApplication.class, args );
+    }
+
+}
