b428516bb18e0aece656f0ca42f50c63b56e52cc
Wp666 <18717792412@163.com>
2020-06-01 14:01:33 +0800

    java代码中调用python脚本

--- spring-cache-sample/src/main/java/com/wp/service/CacheService.java
+++ spring-cache-sample/src/main/java/com/wp/service/CacheService.java
@@ -4,6 +4,9 @@ import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.concurrent.CompletionService;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -24,5 +27,28 @@ public class CacheService {
             e.printStackTrace();
         }
         return "this is hot-cache-value";
+    }
+    public static void main( String[] args ) {
+        String[] arguments = new String[] {"python", "D:/CountingSort.py","2,7,1,10,4"};
+        try {
+
+            Process process = Runtime.getRuntime().exec(arguments);
+            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream(), "GBK"));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+            in.close();
+            //java代码中的process.waitFor()返回值为0表示我们调用python脚本成功，
+            //返回值为1表示调用python脚本失败，这和我们通常意义上见到的0与1定义正好相反
+            int re = process.waitFor();
+            BufferedReader error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            while ((line = error.readLine()) != null) {
+                System.out.println(line);
+            }
+            System.out.println(re);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
