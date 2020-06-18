d0297ff9ffbc871a9f750b821f4f539236512a60
wanpeng <18717792412@163.com>
2020-06-18 19:43:21 +0800

    操作hadoop示例

---
+++ spring-hadoop-sample/src/main/java/com/wp/SpringHadoopSampleApplication.java
@@ -0,0 +1,13 @@
+package com.wp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHadoopSampleApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( SpringHadoopSampleApplication.class, args );
+    }
+
+}
